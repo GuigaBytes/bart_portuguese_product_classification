d19cc67bc9e3c837c9b87465bf98c5e0f4e86ac3
Guilherme Lourenço <93779654+guilhermeSuporteDev@users.noreply.github.com>
2023-11-14 23:11:38 -0300

    init

---
+++ dataset/__init__.py
@@ -0,0 +1,3 @@
+from ._categories import categories
+from ._training_dataset import training_products
+from ._validation_dataset import validation_products
---
+++ dataset/_categories.py
@@ -0,0 +1,16 @@
+categories = [
+    "horti-fruti",
+    "laticínios",
+    "doces",
+    "carnes",
+    "grãos e cereais",
+    "não perecível",
+    "padaria",
+    "bebidas",
+    "higiene",
+    "limpeza",
+    "eletrônicos",
+    "congelados",
+    "frios",
+    "peixaria"
+]
